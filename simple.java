6655f4b2a0e6d26134ec13a1e4bc24f5bac150a3
surendra <surendra.sanjeev@gmail.com>
2019-03-13 11:55:50 -0700

    2nd file

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World surendra " in the terminal window.
+      // Prints "Hello, World surendra s " in the terminal window.
       System.out.println("Hello, World");
    }
 }
